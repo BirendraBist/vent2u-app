c4a25f25a56dc7c315d5ca0d8ac95990dbe88ea7
Ganga Ammay <gang0029@gmail.com>
2021-05-15 20:28:27 +0200

    popover component

---
+++ src/app/pages/preference-history/preference-history.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preference-history',
+  templateUrl: './preference-history.page.html',
+  styleUrls: ['./preference-history.page.scss'],
+})
+export class PreferenceHistoryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/zones/zones.page.ts
+++ src/app/pages/zones/zones.page.ts
@@ -1,15 +1,26 @@
+import { PopoverComponent } from './../../popover/popover.component';
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-zones',
   templateUrl: './zones.page.html',
   styleUrls: ['./zones.page.scss'],
 })
-export class ZonesPage implements OnInit {
+export class ZonesPage  {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public popoverController: PopoverController) { }
+  async presentPopover(eve) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      componentProps:{},
+      cssClass: 'my-custom-class',
+      event: eve,
+      translucent: true
+    });
+    await popover.present();
   }
 
 }
