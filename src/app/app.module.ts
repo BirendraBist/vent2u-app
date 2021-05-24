bcbf28a96dfea18b9e53828baa39137d9892e6a0
oscarbagger <oscarbagger@live.dk>
2021-05-24 18:56:08 +0200

    deployment fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 
 // import for firebase
 import { environment } from '../environments/environment';
@@ -19,28 +16,28 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFirestore } from '@angular/fire/firestore';
-
-
-
-
+import { firestore } from 'firebase';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(),
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFireDatabaseModule,
     AppRoutingModule,
-    HttpClientModule],
+    HttpClientModule,
+  ],
   providers: [
     HttpClient,
     AngularFirestore,
-    
 
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
 
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
